c31afaf904bfded127c70faeec0229e7e289ad88
“xsalina” <419593547@qq.comgit config --global user.email 419593547@qq.com>
2019-07-19 16:49:31 +0800

    react-hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+  const [count,setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
+        <button onClick={()=> setCount (count - 1)}>减去</button>
+          <button onClick={()=> setCount (count + 1)}>加上</button>
+          <p>you click {count} times</p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
