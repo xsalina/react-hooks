54236312c06a4942f53a1a63000678654c31582a
xSalina <419593547@qq.com>
2020-10-07 15:48:19 +0800

    useRef获取元素

---
+++ src/components/useRef1.jsx
@@ -0,0 +1,13 @@
+import React,{useState,useEffect,useRef} from 'react';
+export default (props) => {
+
+    const [isWidth,setIsWidth] = useState('点击按钮获取容器宽度')
+    const contariner = useRef(null)
+    const changeValue = () => {
+        setIsWidth(`容器宽度为${contariner.current.clientWidth}px`)
+    }
+    return <div className='container' ref={contariner}>
+        <h3>{isWidth}</h3>
+        <button onClick={() => changeValue()}>点击按钮获取容器宽度</button>
+    </div>
+}
